2db2c405a1ec3d2ff32ad26d4e32df3ea6e025be
Dima L <leonovdima1998@gmail.com>
2023-09-12 14:00:27 +0300

    change ci file

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ def browser():
     options.add_argument("--headless")
     options.add_argument("--start-maximized")
     options.add_argument("--disable-extensions")
-    driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.get('https://demoqa.com/')
     yield driver
